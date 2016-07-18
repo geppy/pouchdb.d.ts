59a82c5ecfbc9b70df8dcb4c8b1935c9cfdc41bf
geppy <mail@geppy.me>
2016-07-18 15:53:13 -0500

    Test specifying adapters.

--- src/pouchdb-adapter-websql/pouchdb-adapter-websql-tests.ts
+++ src/pouchdb-adapter-websql/pouchdb-adapter-websql-tests.ts
@@ -1,13 +1,29 @@
 /// <reference path="../pouchdb-core/pouchdb-core.d.ts" />
 /// <reference path="./pouchdb-adapter-websql.d.ts" />
 
-function testDbCreation() {
-    const basicDB = new PouchDB('basic');
-    const persistentDb = new PouchDB('unsized', {
-        adapter: 'websql'
-    });
-    const temporaryDb = new PouchDB('sized', {
-        adapter: 'websql',
-        size: 10
-    });
+namespace PouchDBAdapterWebSQLTests {
+    function isBoolean(someBoolean: boolean) {
+    }
+    function isNumber(someNumber: number) {
+    }
+    function isString(someString: string) {
+    }
+
+    function testConstructor() {
+        type MyModel = { numericProperty: number };
+
+        let db = new PouchDB('basic');
+        db = new PouchDB(null, {
+            adapter: 'websql'
+        });
+        db = new PouchDB('sized', {
+            adapter: 'websql',
+            size: 10
+        });
+
+        db.info().then((info) => {
+            isBoolean(info.sqlite_plugin);
+            isString(info.websql_encoding);
+        });
+    }
 }
